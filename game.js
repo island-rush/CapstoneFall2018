39ca3f4a757721644e880a877f8f36cdf42ad2f2
spenceradolph <c19spencer.adolph@usafa.edu>
2018-07-28 01:02:30 -0600

    beautiful event handling now, stopping child to parent propagation

--- game.js
+++ game.js
@@ -1,12 +1,33 @@
 //Javascript Functions used by the Island Rush Game
 //Created by C1C Spencer Adolph (7/28/2018)
 
-function showIsland(event, callingElement) {
+
+function clickIsland(event, callingElement) {
     event.preventDefault();
+    hideIslands();
     document.getElementsByClassName(callingElement.id)[0].style.display = "block";
+    callingElement.style.zIndex = 20;  //default for a gridblock is 10
     event.stopPropagation();
 }
 
 
 
+function clickWater(event, callingElement) {
+    event.preventDefault();
+
+    hideIslands();
+
+    event.stopPropagation();
+}
+
+
+
+function hideIslands() {
+    let x = document.getElementsByClassName("bigblock3x3");
+    let i;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+        x[i].parentNode.style.zIndex = 10;  //10 is the default
+    }
+}
 
