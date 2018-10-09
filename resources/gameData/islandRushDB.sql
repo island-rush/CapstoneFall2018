d0ddf3f43acc0faf55afdc3bda3c4d9436846ece
spenceradolph <c19spencer.adolph@usafa.edu>
2018-10-08 19:20:45 -0600

    sql and favicon

--- resources/gameData/islandRushDB.sql
+++ resources/gameData/islandRushDB.sql
@@ -26,7 +26,7 @@ CREATE TABLE IF NOT EXISTS `games`(
   `gameBlueJoined` int(1) NOT NULL DEFAULT 0,
   `gameBattleSection` varchar(20) NOT NULL DEFAULT 'none',  -- "none" (no popup), "attack", "counter", "askRepeat"......"selectPos", "selectPieces"?
   `gameBattleSubSection` varchar(20) NOT NULL DEFAULT 'choosing_pieces', -- "choosing_pieces", "attacked_popup", "defense_popup"
-  `gameBattleTurn` int(3) NOT NULL DEFAULT 0,  -- put in to kick out aircraft after 2 turns
+  `gameBattleTurn` int(3) NOT NULL DEFAULT 79,  -- put in to kick out aircraft after 2 turns
   `gameBattleLastRoll` int(1) NOT NULL DEFAULT 0, -- 0 for default (or no roll to display anymore/reset), 1-6 for roll
   `gameBattleLastMessage` varchar(50) DEFAULT '', -- used for explaining what happened "red killed blue's fighter with fighter" ex...
   `gameBattlePosSelected` int(4) NOT NULL DEFAULT 999999, -- positionId chosen by attacker (999999 default)
@@ -47,8 +47,8 @@ CREATE TABLE IF NOT EXISTS `games`(
     PRIMARY KEY(`gameId`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
 -- Insert games into the database
-INSERT INTO `games` VALUES (1, 'M1A1', 'Adolph', '5f4dcc3b5aa765d61d8327deb882cf99', 'Blue', 0, 1, 10, 60, 0, 0, 0, 0, 'none', 'choosing_pieces', 0, 'test message', 999999, 'Red', 'Red', 'Red', 'Red', 'Red', 'Red', 'Red', 'Red', 'Red', 'Red', 'Red', 'Red', 'Red', 'Blue');
-INSERT INTO `games` VALUES (2, 'T1A1', 'Kulp', '5f4dcc3b5aa765d61d8327deb882cf99', 'Blue', 0, 1, 100, 20, 0, 0, 0, 0, 'none', 'choosing_pieces', 0, 'test message', 999999, 'Red', 'Red', 'Red', 'Red', 'Red', 'Red', 'Red', 'Red', 'Red', 'Red', 'Red', 'Red', 'Red', 'Blue');
+INSERT INTO `games` VALUES (1, 'M1A1', 'Adolph', '5f4dcc3b5aa765d61d8327deb882cf99', 'Blue', 0, 1, 10, 60, 0, 0, 0, 0, 'none', 'choosing_pieces', 0, 0, 'test message', 999999, 'Red', 'Red', 'Red', 'Red', 'Red', 'Red', 'Red', 'Red', 'Red', 'Red', 'Red', 'Red', 'Red', 'Blue');
+INSERT INTO `games` VALUES (2, 'T1A1', 'Kulp', '5f4dcc3b5aa765d61d8327deb882cf99', 'Blue', 0, 1, 100, 20, 0, 0, 0, 0, 'none', 'choosing_pieces', 0, 0, 'test message', 999999, 'Red', 'Red', 'Red', 'Red', 'Red', 'Red', 'Red', 'Red', 'Red', 'Red', 'Red', 'Red', 'Red', 'Blue');
 
 
 -- Table of Units (static)
@@ -129,6 +129,7 @@ CREATE TABLE IF NOT EXISTS `updates`(
 	`updatePlacementId` int(4) DEFAULT 0,
 	`updateNewPositionId` int(4) DEFAULT 0,
 	`updateNewContainerId` int(4) DEFAULT 0,
+    `updateNewMoves` int(2) DEFAULT 9,
 	`updateNewUnitId` int(4) DEFAULT 16,
     `updateBattlePieceState` int(2) DEFAULT 8,
     `updateBattlePositionSelectedPieces` varchar(16000) DEFAULT 'defaultString',
